53653fb65f27e242ad7ac956aa515619663362c0
Elior Ben-Yosef <eliorby@gmail.com>
2019-08-28 13:57:10 +0300

    CHANGED minor update on policy evaluation methods (MC, TD0).

--- tabular_methods/rl_tabular_testing.py
+++ tabular_methods/rl_tabular_testing.py
@@ -6,67 +6,85 @@ from tabular_methods.rl_tabular import TabularMethods
 class AlgorithmsTesting:
 
     @staticmethod
-    def test_td0_policy_evaluation(episodes):
-        method_name = 'TD(0) Policy Evaluation'
-
+    def test_td0_policy_evaluation(episodes, print_v_table):
+        # Mountain Car:
         car_vel_bin_num = 34  # ~100% success rate (32 rarely loses)
         mountain_car_env_single_state_space = Envs_DSS.MountainCar(Envs_DSS.MountainCar.CAR_VEL,
                                                                    car_vel_bin_num=car_vel_bin_num)
-        mountain_car_td_zero_model = TabularMethods.TdZeroModel(mountain_car_env_single_state_space, episodes=episodes)
+        mountain_car_td0_model = TabularMethods.TdZeroModel(mountain_car_env_single_state_space, episodes=episodes)
         # possible actions: backward (0), none (1), forward (2)
         mountain_car_policy = lambda velocity_state: 0 if velocity_state < (car_vel_bin_num//2) else 2
-        mountain_car_td_zero_model.perform_td0_policy_evaluation(mountain_car_policy)
+        mountain_car_td0_model.perform_td0_policy_evaluation(mountain_car_policy, print_info=print_v_table)
 
+        # Cart Pole:
         pole_theta_bin_num = 10
         cart_pole_env_single_state_space = Envs_DSS.CartPole(Envs_DSS.CartPole.POLE_THETA,
                                                              pole_theta_bin_num=pole_theta_bin_num)
-        cart_pole_td_zero_model = TabularMethods.TdZeroModel(cart_pole_env_single_state_space, episodes=episodes)
+        cart_pole_td0_model = TabularMethods.TdZeroModel(cart_pole_env_single_state_space, episodes=episodes)
         # possible actions: left (0), right (1)
         cart_pole_policy = lambda theta_state: 0 if theta_state < (pole_theta_bin_num//2) else 1
-        cart_pole_td_zero_model.perform_td0_policy_evaluation(cart_pole_policy)
+        cart_pole_td0_model.perform_td0_policy_evaluation(cart_pole_policy, print_info=print_v_table)
+
+        # Frozen Lake:
+        frozen_lake_env = Envs_DSS.FrozenLake()
+        frozen_lake_td0_model = TabularMethods.TdZeroModel(frozen_lake_env, episodes=episodes)
+        # possible actions: left (0), right (1)
+        frozen_lake_policy = lambda s: frozen_lake_env.env.action_space.sample()  # random policy
+        frozen_lake_td0_model.perform_td0_policy_evaluation(frozen_lake_policy, print_info=print_v_table)
 
     @staticmethod
-    def test_mc_policy_evaluation(episodes):
-        method_name = 'MC Policy Evaluation'
-
-        frozen_lake_env = Envs_DSS.FrozenLake()
-        frozen_lake_monte_carlo_model = TabularMethods.MonteCarloModel(
-            frozen_lake_env, episodes=episodes, eps_dec=2/episodes
-        )
-        frozen_lake_policy = lambda s: frozen_lake_env.env.action_space.sample()  # random policy
-        frozen_lake_monte_carlo_model.perform_MC_policy_evaluation(frozen_lake_policy)
-
+    def test_mc_policy_evaluation(episodes, print_v_table):
+        # Mountain Car:
         car_vel_bin_num = 34  # ~100% success rate (32 rarely loses)
         mountain_car_env_single_state_space = Envs_DSS.MountainCar(Envs_DSS.MountainCar.CAR_VEL,
                                                                    car_vel_bin_num=car_vel_bin_num)
-        mountain_car_monte_carlo_model = TabularMethods.MonteCarloModel(
+        mountain_car_mc_model = TabularMethods.MonteCarloModel(
             mountain_car_env_single_state_space, episodes=episodes, eps_dec=2/episodes
         )
         # possible actions: backward (0), none (1), forward (2)
         mountain_car_policy = lambda velocity_state: 0 if velocity_state < (car_vel_bin_num//2) else 2
-        mountain_car_monte_carlo_model.perform_MC_policy_evaluation(mountain_car_policy)
+        mountain_car_mc_model.perform_MC_policy_evaluation(mountain_car_policy, print_info=print_v_table)
+
+        # Cart Pole:
+        pole_theta_bin_num = 10
+        cart_pole_env_single_state_space = Envs_DSS.CartPole(Envs_DSS.CartPole.POLE_THETA,
+                                                             pole_theta_bin_num=pole_theta_bin_num)
+        cart_pole_mc_model = TabularMethods.MonteCarloModel(
+            cart_pole_env_single_state_space, episodes=episodes, eps_dec=2/episodes)
+        # possible actions: left (0), right (1)
+        cart_pole_policy = lambda theta_state: 0 if theta_state < (pole_theta_bin_num // 2) else 1
+        cart_pole_mc_model.perform_MC_policy_evaluation(cart_pole_policy, print_info=print_v_table)
+
+        # Frozen Lake:
+        frozen_lake_env = Envs_DSS.FrozenLake()
+        frozen_lake_mc_model = TabularMethods.MonteCarloModel(
+            frozen_lake_env, episodes=episodes, eps_dec=2/episodes
+        )
+        frozen_lake_policy = lambda s: frozen_lake_env.env.action_space.sample()  # random policy
+        frozen_lake_mc_model.perform_MC_policy_evaluation(frozen_lake_policy, print_info=print_v_table)
+
 
     @staticmethod
     def test_mc_non_exploring_starts_control(episodes):
         method_name = 'MC non-exploring starts'
 
         frozen_lake_env = Envs_DSS.FrozenLake()
-        frozen_lake_monte_carlo_model = TabularMethods.MonteCarloModel(
+        frozen_lake_mc_model = TabularMethods.MonteCarloModel(
             frozen_lake_env, episodes=episodes, eps_max=frozen_lake_env.EPS_MIN
         )
         frozen_lake_total_rewards_mc_without_es, frozen_lake_accumulated_rewards_mc_without_es = \
-            frozen_lake_monte_carlo_model.perform_MC_non_exploring_starts_control(print_info=True)
+            frozen_lake_mc_model.perform_MC_non_exploring_starts_control(print_info=True)
         # Utils.plot_running_average(method_name + ' - ' + frozen_lake_env.name,
         #                            frozen_lake_total_rewards_mc_without_es)
         Utils.plot_accumulated_rewards(method_name + ' - ' + frozen_lake_env.name,
                                        frozen_lake_accumulated_rewards_mc_without_es)
 
         blackjack_env = Envs_DSS.Blackjack()
-        blackjack_monte_carlo_model = TabularMethods.MonteCarloModel(
+        blackjack_mc_model = TabularMethods.MonteCarloModel(
             blackjack_env, episodes=episodes, eps_max=0.05, eps_dec=1e-7
         )
         blackjack_total_rewards_mc_without_es, blackjack_accumulated_rewards_mc_without_es = \
-            blackjack_monte_carlo_model.perform_MC_non_exploring_starts_control(print_info=True)
+            blackjack_mc_model.perform_MC_non_exploring_starts_control(print_info=True)
         # Utils.plot_running_average(method_name + ' - ' + blackjack_env.name,
         #                            blackjack_total_rewards_mc_without_es)
         Utils.plot_accumulated_rewards(method_name + ' - ' + blackjack_env.name,
@@ -85,11 +103,11 @@ class AlgorithmsTesting:
         method_name = 'Off-policy MC Control'
 
         blackjack_env = Envs_DSS.Blackjack()
-        blackjack_monte_carlo_model = TabularMethods.MonteCarloModel(
+        blackjack_mc_model = TabularMethods.MonteCarloModel(
             blackjack_env, episodes=episodes, eps_max=0.05, eps_dec=1e-7
         )
         blackjack_total_rewards_off_policy_mc, blackjack_accumulated_rewards_off_policy_mc = \
-            blackjack_monte_carlo_model.perform_off_policy_MC_control(print_info=True)
+            blackjack_mc_model.perform_off_policy_MC_control(print_info=True)
         # Utils.plot_running_average(method_name + ' - ' + blackjack_env.name,
         #                            blackjack_total_rewards_off_policy_mc)
         Utils.plot_accumulated_rewards(method_name + ' - ' + blackjack_env.name,
@@ -172,11 +190,11 @@ class EnvironmentsTesting:
     def test_frozen_lake(episodes):
         frozen_lake_env = Envs_DSS.FrozenLake()
 
-        monte_carlo_model_01 = TabularMethods.MonteCarloModel(
+        mc_model_01 = TabularMethods.MonteCarloModel(
             frozen_lake_env, episodes=episodes, eps_max=frozen_lake_env.EPS_MIN
         )
         total_rewards_mc_without_es, accumulated_rewards_mc_without_es = \
-            monte_carlo_model_01.perform_MC_non_exploring_starts_control(print_info=True)
+            mc_model_01.perform_MC_non_exploring_starts_control(print_info=True)
         # Utils.plot_running_average('Frozen Lake', total_rewards_mc_without_es)          # less preferred
         Utils.plot_accumulated_rewards('Frozen Lake', accumulated_rewards_mc_without_es)  # better
 
@@ -204,17 +222,17 @@ class EnvironmentsTesting:
     def test_blackjack(episodes):
         blackjack_env = Envs_DSS.Blackjack()
 
-        monte_carlo_model_01 = TabularMethods.MonteCarloModel(
+        mc_model_01 = TabularMethods.MonteCarloModel(
             blackjack_env, episodes=episodes, eps_max=0.05, eps_dec=1e-7
         )
         total_rewards_mc_without_es, accumulated_rewards_mc_without_es = \
-            monte_carlo_model_01.perform_MC_non_exploring_starts_control(print_info=True)
+            mc_model_01.perform_MC_non_exploring_starts_control(print_info=True)
 
-        monte_carlo_model_02 = TabularMethods.MonteCarloModel(
+        mc_model_02 = TabularMethods.MonteCarloModel(
             blackjack_env, episodes=episodes, eps_max=0.05, eps_dec=1e-7
         )
         total_rewards_off_policy_mc, accumulated_rewards_off_policy_mc = \
-            monte_carlo_model_02.perform_off_policy_MC_control(print_info=True)
+            mc_model_02.perform_off_policy_MC_control(print_info=True)
 
         # less preferred:
         # total_rewards_list = [total_rewards_mc_without_es, total_rewards_off_policy_mc]
@@ -273,8 +291,8 @@ def algorithms_test():
     AlgorithmsTesting.test_double_q_learning(1000)
 
     # policy_evaluation methods:
-    AlgorithmsTesting.test_td0_policy_evaluation(10)
-    AlgorithmsTesting.test_mc_policy_evaluation(10)
+    AlgorithmsTesting.test_td0_policy_evaluation(10, print_v_table=True)
+    AlgorithmsTesting.test_mc_policy_evaluation(10, print_v_table=True)
 
 
 def environments_test():
