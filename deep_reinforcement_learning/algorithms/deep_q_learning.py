bb2f310b29c375a6272bc9c73ea9e81e78e63ca4
Elior Ben-Yosef <eliorby@gmail.com>
2019-09-12 00:06:33 +0300

    ADDED autotune_hyperparams in deep_q_learning

--- deep_reinforcement_learning/algorithms/deep_q_learning.py
+++ deep_reinforcement_learning/algorithms/deep_q_learning.py
@@ -615,5 +615,72 @@ def play(env_type, lib_type=utils.LIBRARY_TF, enable_models_saving=False, load_c
     train(custom_env, agent, n_episodes, enable_models_saving)
 
 
+def autotune_hyperparams(lib_type=utils.LIBRARY_TF, enable_models_saving=False, load_checkpoint=False,
+                         perform_random_gameplay=False):
+
+    # argparse is a built-in module for python that allows us to parse command-line options.
+    #   to run it from cmd type: python deep_q_learning.py -n_episodes 100 ...
+    #   to run multiple times, put && in between the commands
+
+    # the parser will parse command-line options from cmd text into str \ int\ float \ ...
+    parser = argparse.ArgumentParser(description='Command-line Utility for training RL models')
+
+    # The hyphen makes the argument optional (no hyphen makes it a required option)
+    #   You can add help='explanation'.
+
+    parser.add_argument('-n_episodes', type=int, default=1)  # 500
+
+    parser.add_argument('-optimizer', type=int, default='adam')
+    parser.add_argument('-alpha', type=float, default=0.001, help='Learning rate for optimizer')  # 0.0005, 0.003 ?
+
+    parser.add_argument('-fc1_dim', type=int, default=256)
+    parser.add_argument('-fc2_dim', type=int, default=256)
+
+    parser.add_argument('-gamma', type=float, default=0.99, help='Discount factor for update equation')
+
+    # in epsilon-greedy action selection:
+    # parser.add_argument('-eps_max', type=float, default=1.0)
+    parser.add_argument('-eps_min', type=float, default=0.01)  # EPS_MIN = None
+    # parser.add_argument('-eps_dec', type=float, default=0.996)  # (max - min) * 2 / episodes
+
+    parser.add_argument('-memory_size', type=int, default=1000000)
+    parser.add_argument('-memory_batch_size', type=int, default=64)
+
+    args = parser.parse_args()
+
+    custom_env = Envs.ClassicControl.CartPole()
+    custom_env.env.seed(28)
+
+    if args.optimizer == 'sgd':
+        optimizer = utils.OPTIMIZER_SGD
+    elif args.optimizer == 'adagrad':
+        optimizer = utils.OPTIMIZER_Adagrad
+    elif args.optimizer == 'adadelta':
+        optimizer = utils.OPTIMIZER_Adadelta
+    elif args.optimizer == 'rmsprop':
+        optimizer = utils.OPTIMIZER_RMSprop
+    else:  # args.optimizer == 'adam'
+        optimizer = utils.OPTIMIZER_Adam
+
+    agent = Agent(
+        custom_env, [args.fc1_dim, args.fc2_dim], args.n_episodes,
+        args.alpha, optimizer_type=optimizer, gamma=args.gamma,
+        eps_min=args.eps_min, eps_dec=args.eps_dec,  # eps_max=args.eps_max,
+        memory_size=args.memory_size, memory_batch_size=args.memory_batch_size,
+        double_dql=False, tau=None, lib_type=lib_type
+    )
+
+    if enable_models_saving and load_checkpoint:
+        agent.load_models()
+
+    if perform_random_gameplay:
+        # the agent's memory is originally initialized with zeros (which is perfectly acceptable).
+        # however, we can overwrite these zeros with actual gameplay sampled from the environment.
+        load_up_agent_memory_with_random_gameplay(custom_env, agent)
+
+    train(custom_env, agent, args.n_episodes, enable_models_saving)
+
+
 if __name__ == '__main__':
-    play(0, lib_type=utils.LIBRARY_TF)         # CartPole (0), Breakout (1), SpaceInvaders (2)
+    # play(0, lib_type=utils.LIBRARY_TF)         # CartPole (0), Breakout (1), SpaceInvaders (2)
+    autotune_hyperparams()
