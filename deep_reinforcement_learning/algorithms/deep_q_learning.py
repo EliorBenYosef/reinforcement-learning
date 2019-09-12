a02c844e05eb221ee29bf9eb4fd7ac99677fd025
Elior Ben-Yosef <eliorby@gmail.com>
2019-09-12 23:20:25 +0300

    FIXED perform_grid_search()

--- deep_reinforcement_learning/algorithms/deep_q_learning.py
+++ deep_reinforcement_learning/algorithms/deep_q_learning.py
@@ -692,29 +692,47 @@ def perform_grid_search(lib_type=utils.LIBRARY_TF, enable_models_saving=False, l
 
     custom_env = Envs.ClassicControl.CartPole()
     custom_env.env.seed(28)
-    n_episodes = 10  # 500
+    n_episodes = 3000
 
     # double_dql = False
     # tau = None
 
     ###########################################
 
-    optimizer_list = [utils.OPTIMIZER_SGD, utils.OPTIMIZER_Adagrad, utils.OPTIMIZER_Adadelta, utils.OPTIMIZER_RMSprop,
-                      utils.OPTIMIZER_Adam]
-    alpha_list = [0.0005, 0.001, 0.002, 0.004]
+    # optimizer_list = [utils.OPTIMIZER_SGD, utils.OPTIMIZER_Adagrad, utils.OPTIMIZER_Adadelta, utils.OPTIMIZER_RMSprop,
+    #                   utils.OPTIMIZER_Adam]
+    # alpha_list = [0.0005, 0.001, 0.002, 0.004]
+    #
+    # fc1_dim_list = [64, 128, 256, 512]
+    # fc2_dim_list = [64, 128, 256, 512]
+    #
+    # gamma_list = [0.9, 0.95, 0.99, 1.0]  # [0.9, 0.95, 0.99, 1.0]
+    #
+    # eps_max_list = [1.0]
+    # eps_min_list = [0.0, 0.01, 0.02, 0.04]
+    # eps_dec_list = [0.1, 0.2, 0.3, 0.4]  # (1.0 - min) * 2 / n_episodes  # 0.996
+    # # eps_dec_type = utils.EPS_DEC_LINEAR,
+    #
+    # memory_size_list = [10000, 100000, 1000000]
+    # memory_batch_size_list = [8, 16, 32, 64]
 
-    fc1_dim_list = [64, 128, 256, 512]
-    fc2_dim_list = [64, 128, 256, 512]
+    ###########################################
 
-    gamma_list = [0.9, 0.95, 0.99, 1.0]  # [0.9, 0.95, 0.99, 1.0]
-
-    eps_max_list = [1.0]
-    eps_min_list = [0.0, 0.01, 0.02, 0.04]
-    eps_dec_list = [0.1, 0.2, 0.3, 0.4]  # (1.0 - min) * 2 / n_episodes[0]  # 0.996
-    # eps_dec_type = utils.EPS_DEC_LINEAR,
-
-    memory_size_list = [10000, 100000, 1000000]
-    memory_batch_size_list = [8, 16, 32, 64]
+    # optimizer_list = [utils.OPTIMIZER_Adam]
+    # alpha_list = [0.0005]
+    #
+    # fc1_dim_list = [256]
+    # fc2_dim_list = [256]
+    #
+    # gamma_list = [0.99]
+    #
+    # eps_max_list = [1.0]
+    # eps_min_list = [0.1]
+    # eps_dec_list = [1.8 / n_episodes]  # (1.0 - min) * 2 / n_episodes  # 0.996
+    # # eps_dec_type = utils.EPS_DEC_LINEAR,
+    #
+    # memory_size_list = [1000000]
+    # memory_batch_size_list = [64]
 
     ###########################################
 
@@ -728,7 +746,7 @@ def perform_grid_search(lib_type=utils.LIBRARY_TF, enable_models_saving=False, l
 
     eps_max_list = [1.0]
     eps_min_list = [0.1]
-    eps_dec_list = [0.1]  # (1.0 - min) * 2 / n_episodes[0]  # 0.996
+    eps_dec_list = [1.8 / n_episodes]  # (1.0 - min) * 2 / n_episodes  # 0.996
     # eps_dec_type = utils.EPS_DEC_LINEAR,
 
     memory_size_list = [1000000]
@@ -738,6 +756,8 @@ def perform_grid_search(lib_type=utils.LIBRARY_TF, enable_models_saving=False, l
 
     scores_histories = []
     labels = []
+
+    counter = 0
 
     for optimizer in optimizer_list:
         for alpha in alpha_list:
@@ -749,6 +769,8 @@ def perform_grid_search(lib_type=utils.LIBRARY_TF, enable_models_saving=False, l
                                 for eps_dec in eps_dec_list:
                                     for memory_size in memory_size_list:
                                         for memory_batch_size in memory_batch_size_list:
+                                            counter += 1
+                                            print('\n', 'Iteration %d' % counter, '\n')
 
                                             agent = Agent(
                                                 custom_env, [fc1_dim, fc2_dim], n_episodes,
